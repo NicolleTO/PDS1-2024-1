58b7d48b076bd50ce4a15f9b60bb6c970978bcd7
Nicolle Oliveira <129110665+NicolleTO@users.noreply.github.com>
2024-05-07 17:10:14 -0300

    Update main.c

--- Trabalho Prático 1/main.c
+++ Trabalho Prático 1/main.c
@@ -2,10 +2,32 @@
 #include <stdlib.h>
 
 #define SUCESSO 0
+#define MAX 100
+
+typedef struct pokemon{
+    char nome[MAX];
+    int ataque;
+    int defesa;
+    int vida;
+    char tipo[MAX];
+}pokemon;
 
 int main(int argc, char** argv) {
     
-    //O inicio...
+    struct pokemon treinador1[MAX], treinador2[MAX];
+    int n1, n2;
+    FILE* arquivo;
+    arquivo = fopen("arquivo.txt", "a");
+    
+    if(arquivo != NULL){
+        printf("sucesso0");
+    }else{
+        printf("Algo deu errado");
+        return SUCESSO;
+    }
+    
+    fclose(arquivo);
+    
     
     return SUCESSO;
 }
