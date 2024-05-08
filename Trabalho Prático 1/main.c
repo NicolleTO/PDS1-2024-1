a8690b38bbf89c52420b18a0a664a6c702154f5a
Nicolle Oliveira <129110665+NicolleTO@users.noreply.github.com>
2024-05-08 17:25:14 -0300

    Update main.c

--- Trabalho Prático 1/main.c
+++ Trabalho Prático 1/main.c
@@ -31,3 +31,51 @@ int main(int argc, char** argv) {
     
     return SUCESSO;
 }
+//-------------------------------------------------------------------------------------
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUCESSO 0
+#define MAX 100
+
+typedef struct pokemon{
+    char nome[MAX];
+    int ataque;
+    int defesa;
+    int vida;
+    char tipo[MAX];
+}pokemon;
+
+int main(int argc, char** argv) {
+    
+    struct pokemon treinador1[MAX], treinador2[MAX];
+    int n1 = 0, n2 = 0;
+    char esse[MAX];
+    FILE* arquivo;
+    arquivo = fopen("arquivo.txt", "r");
+    
+    if(arquivo != NULL){
+        printf("sucesso0\n");
+        
+        fscanf(arquivo, "%d %d", &n1, &n2);
+        
+        fclose(arquivo);
+    }else{
+        printf("Algo deu errado");
+        return SUCESSO;
+    }
+    
+    for(int i = 0; i < n1; i++){
+        treinador1[i].nome = "pokemon\0";
+        printf("%s\n", treinador1[i].nome);
+    }
+    for(int i = 0; i < n2; i++){
+        treinador2[i].nome = "temos\0";
+        printf("%s\n", treinador2[i].nome);
+        
+    }
+    
+    return SUCESSO;
+}
+*/
