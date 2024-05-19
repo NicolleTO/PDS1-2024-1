ebf6a83df2c54f0c3c93daa015d41cbbf226fc81
Nicolle Oliveira <129110665+NicolleTO@users.noreply.github.com>
2024-05-19 20:33:30 -0300

    Create 8.3 - Cadastro de Produtos.c

---
+++ Exercicios Lab/8.3 - Cadastro de Produtos.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SUCESSO 0
+#define FALHA 1
+#define MAX_TAM 50
+#define MAX_PRODUTOS 8
+
+typedef struct produto{
+    char nome[MAX_TAM];
+    char marca[MAX_TAM];
+    float preco;
+}produto;
+
+typedef struct marca{
+    char nome[MAX_TAM];
+    int quant_produtos;
+    float media_preco;
+}marca;
+
+int encontra_marcas(struct produto produtos[], struct marca marcas[]){
+    
+    int n = 0;
+    int existe = FALHA;
+    
+    for(int i = 0; i < MAX_PRODUTOS; i++){
+        for(int j = 0; j < MAX_PRODUTOS; j++){
+            if(strcmp(produtos[i].marca, marcas[j].nome) == SUCESSO){
+                existe = SUCESSO;
+                break;
+            }
+        }
+        
+        if(existe != SUCESSO){
+            strcmp(marcas[n].nome, produtos[i].marca);
+        }
+        
+        existe = FALHA;
+    }
+    return n;
+}
+
+int main(int argc, char** argv) {
+    
+    struct produto produtos[MAX_PRODUTOS];
+    struct marca marcas[MAX_PRODUTOS];
+    int n_marcas = 0;
+    
+    //Ler as informacoes dos produtos
+    for(int i = 0; i < MAX_PRODUTOS; i++){
+        scanf("%s %s %f", produtos[i].nome, produtos[i].marca, &produtos[i].preco);
+        getchar();
+    }
+    
+    n_marcas = encontra_marcas(produtos, marcas);
+    
+    printf("%d marcas\n", n_marcas);
+    
+    for(int i = 0; i < n_marcas; i++){
+        printf("%s\n", marcas[i].nome);
+    }
+    
+    return SUCESSO;
+}
