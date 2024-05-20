387f9a9d42e750fd6334914c035586f91701a2c0
Nicolle Oliveira <129110665+NicolleTO@users.noreply.github.com>
2024-05-19 21:25:23 -0300

    Update 8.3 - Cadastro de Produtos.c

--- Exercicios Lab/8.3 - Cadastro de Produtos.c
+++ Exercicios Lab/8.3 - Cadastro de Produtos.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-a sadia 2
-b sadia 3
-c bic 3
-d sadia 1
-e mac 7
-o bic 1
-p mac 1.2
-u trace 1
-*/
-
 #define SUCESSO 0
 #define FALHA 1
 #define MAX_TAM 50
@@ -26,7 +15,7 @@ typedef struct produto{
 typedef struct marca{
     char nome[MAX_TAM];
     int quant_produtos;
-    float media_preco;
+    float media;
 }marca;
 
 int encontra_marcas(struct produto produtos[], struct marca marcas[]){
@@ -35,7 +24,7 @@ int encontra_marcas(struct produto produtos[], struct marca marcas[]){
     int existe = FALHA;
     
     for(int i = 0; i < MAX_PRODUTOS; i++){
-        for(int j = 0; j < MAX_PRODUTOS; j++){
+        for(int j = 0; j < n; j++){
             if(strcmp(produtos[i].marca, marcas[j].nome) == SUCESSO){
                 existe = SUCESSO;
                 break;
@@ -43,7 +32,8 @@ int encontra_marcas(struct produto produtos[], struct marca marcas[]){
         }
         
         if(existe != SUCESSO){
-            strcmp(marcas[n].nome, produtos[i].marca);
+            strcpy(marcas[n].nome, produtos[i].marca);
+            n++;
         }
         
         existe = FALHA;
@@ -51,24 +41,50 @@ int encontra_marcas(struct produto produtos[], struct marca marcas[]){
     return n;
 }
 
+void quant_produtos(struct produto produtos[], struct marca marcas[], int n_marcas){
+    
+    for(int i = 0; i < n_marcas; i++){
+        
+        for(int j = 0; j < MAX_PRODUTOS; j++){
+            if(strcmp(marcas[i].nome, produtos[j].marca) == SUCESSO){
+                marcas[i].quant_produtos++;
+                marcas[i].media += produtos[j].preco;
+            }
+        }
+        
+        marcas[i].media = marcas[i].media / marcas[i].quant_produtos;
+    }
+}
+
 int main(int argc, char** argv) {
     
     struct produto produtos[MAX_PRODUTOS];
     struct marca marcas[MAX_PRODUTOS];
     int n_marcas = 0;
+    float media_t = 0;
     
     //Ler as informacoes dos produtos
     for(int i = 0; i < MAX_PRODUTOS; i++){
         scanf("%s %s %f", produtos[i].nome, produtos[i].marca, &produtos[i].preco);
         getchar();
+        
+        media_t += produtos[i].preco;
+        
+        marcas[i].quant_produtos = 0;
+        marcas[i].media = 0.00;
     }
     
     n_marcas = encontra_marcas(produtos, marcas);
-    
-    printf("%d marcas\n", n_marcas);
+    quant_produtos(produtos, marcas, n_marcas);
     
     for(int i = 0; i < n_marcas; i++){
-        printf("%s\n", marcas[i].nome);
+        printf("%s %d\n", marcas[i].nome, marcas[i].quant_produtos);
+    }
+    
+    printf("media total %.2f\n", media_t / MAX_PRODUTOS);
+    
+    for(int i = 0; i < n_marcas; i++){
+        printf("media %s %.2f\n", marcas[i].nome, marcas[i].media);
     }
     
     return SUCESSO;
