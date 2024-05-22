97cf60ebf29697e25f670118e8c71e52c8da1039
Nicolle Oliveira <129110665+NicolleTO@users.noreply.github.com>
2024-05-22 19:43:42 -0300

    Update 8.4 - O melhor aluno e seu xará.c

--- Exercicios Lab/8.4 - O melhor aluno e seu xará.c
+++ Exercicios Lab/8.4 - O melhor aluno e seu xará.c
@@ -12,7 +12,6 @@ alano
 3
 89 32 43
 */
-
 #include <stdio.h>
 #include <string.h>
 
@@ -20,21 +19,75 @@ alano
 #define FALHA 1
 
 #define MAX_TAM 50
-#define MAX_ALUNOS 2
+#define MAX_ALUNOS 3
 #define QUANT_MATERIAS 3
+
+#define APROVADO (1 == 1)
+#define REPROVADO (!APROVADO)
+#define PONTOS_MIN 60
+
+//importancia
+#define PDS 1
+#define GAAL 2
+#define CALCULO 3
 
 typedef struct aluno{
     char nome[MAX_TAM];
     char sobrenome[MAX_TAM];
     int matricula;
     float notas[QUANT_MATERIAS];
+    float media;
+    int situacao;
 }aluno;
 
+void media(struct aluno turma[]){
+    
+    float valor = 0;
 
+    for(int i = 0; i < MAX_ALUNOS; i++){
+        for(int j = 0; j < QUANT_MATERIAS; j++){
+            valor += turma[i].notas[j];
+        }
+        turma[i].media = valor/QUANT_MATERIAS;
+        valor = 0;
+        if(turma[i].media >= PONTOS_MIN){
+            situacao = APROVADO;
+        }else{
+            situacao = REPROVADO;
+        }
+    }
+}
+
+int melhor(struct aluno turma[]){
+    int esse = 0;
+    
+    for(int i = 0; i < MAX_ALUNOS; i++){
+        if(turma[esse].media < turma[i].media){
+            esse = i;
+        }
+    }
+    return esse;
+}
+
+int pior(struct aluno turma[]){
+    int esse = 0;
+    
+    for(int i = 0; i < MAX_ALUNOS; i++){
+        if(turma[esse].media > turma[i].media){
+            esse = i;
+        }
+    }
+    return esse;
+}
+
+int situacao(struct aluno melhor_aluno, struct aluno pior_aluno){
+    
+}
 
 int main(int argc, char** argv) {
     
     struct aluno turma[MAX_ALUNOS];
+    int melhor_aluno, pior_aluno;
     
     for(int i = 0; i < MAX_ALUNOS; i++){
         scanf("%s %s %d", turma[i].nome, turma[i].sobrenome, &turma[i].matricula);
@@ -44,12 +97,20 @@ int main(int argc, char** argv) {
         }
     }
    
+    media(turma);
+    melhor_aluno = melhor(turma);
+    pior_aluno = pior(turma);
+    
     for(int i = 0; i < MAX_ALUNOS; i++){
-        printf("%s %s\n", turma[i].nome, turma[i].sobrenome);
+        printf("\n%s %s\n", turma[i].nome, turma[i].sobrenome);
         for(int j = 0; j < QUANT_MATERIAS; j++){
-            printf("%f ", turma[i].notas[j]);
+            printf("%.2f ", turma[i].notas[j]);
         }
-        printf("\n");
+        printf("\nmedia - %.2f\n", turma[i].media);
     }
+    
+    printf("\nMelhor aluno - %s %s | Média - %.2f", turma[melhor_aluno].nome, turma[melhor_aluno].sobrenome, turma[melhor_aluno].media);
+    printf("\nPior aluno - %s %s | Média - %.2f\n", turma[pior_aluno].nome, turma[pior_aluno].sobrenome, turma[pior_aluno].media);
+    
     return SUCESSO;
 }
