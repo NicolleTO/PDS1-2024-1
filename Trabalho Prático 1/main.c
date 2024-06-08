5cd8d9613031cd5d9f5976907a222a86a46b626f
Nicolle Oliveira <129110665+NicolleTO@users.noreply.github.com>
2024-06-08 19:43:35 -0300

    Update main.c

--- Trabalho Prático 1/main.c
+++ Trabalho Prático 1/main.c
@@ -4,7 +4,7 @@
 #include "pokemon_battle.h"
 
 #define SUCESSO 0
-#define FALHA 1
+#define FALHA -1
 
 int main(int argc, char** argv) {
     
@@ -21,14 +21,14 @@ int main(int argc, char** argv) {
     }
     
     if(fscanf(arquivo, "%d %d", &trainer_um.num_ativos, &trainer_dois.num_ativos) != 2){
-        printf("**Erro na leitura do numero de pokemons - Variaveis insuficientes**");
+        printf("**Erro na leitura do número de pokemons - Variáveis insuficientes**");
         return FALHA;
     }
     
     erro += le_arquivo(trainer_um.team, trainer_um.num_ativos, arquivo);
     erro += le_arquivo(trainer_dois.team, trainer_dois.num_ativos, arquivo);
     
-    if(erro > SUCESSO){
+    if(erro < SUCESSO){
         return FALHA;
     }
     
@@ -41,29 +41,15 @@ int main(int argc, char** argv) {
     printf("\n");
     
     do{
-        
-        //printf("***************\n%s VS %s\n***************\n", trainer_um.team[trainer_um.pos_em_campo].nome, trainer_dois.team[trainer_dois.pos_em_campo].nome);
-        //printf("turno1\n");
         turno(&trainer_um, &trainer_dois);
-        //printf("turno2\n");
         turno(&trainer_dois, &trainer_um);
         
     }while((trainer_um.num_ativos > GAME_OVER) && (trainer_dois.num_ativos > GAME_OVER));
     
     if(trainer_um.num_ativos != GAME_OVER){
-        j = trainer_um.pos_em_campo;
-        printf("\nJogador 1 venceu!\n\nPokemons sobreviventes:\n");
-        for(int i = 0; i < trainer_um.num_ativos; i++){
-            printf("%s\n",  trainer_um.team[j].nome);
-            j++;
-        }
+       vencedor(trainer_um, 1);
     }else{
-        j = trainer_dois.pos_em_campo;
-        printf("\nJogador 2 venceu!\n\nPokemons sobreviventes:\n");
-        for(int i = 0; i < trainer_dois.num_ativos; i++){
-            printf("%s\n", trainer_dois.team[j].nome);
-            j++;
-        }
+        vencedor(trainer_dois, 2);
     }
     
     printf("\nPokemons derrotados:\n");
